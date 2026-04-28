679e113af88efb3c2a817576c7c835112cd35612
Ghaleb <ghaleb.khadra@gmail.com>
2026-04-27 22:23:10 -0400

    feat: change screen capture from pyautogui's Pillow dependency to mss.MSS() for faster screenshots, replace rgb-based color-check logic with numpy's for faster calculation

--- dino.py
+++ dino.py
@@ -1,16 +1,44 @@
 import pyautogui
+import mss
+import numpy as np
 
+# ==================== INITIAL BRIEF PAUSE ====================
 pyautogui.time.sleep(3)
+# ==================== INITIAL BRIEF PAUSE ====================
 
-while "Dino Bot Running":
-    x, y = pyautogui.position()
-    rgb = pyautogui.pixel(x, y)
+# ==================== SCREEN REGION CONSTANT ====================
+constant = 30   # Padding constant around the cursor position
+# ==================== SCREEN REGION CONSTANT ====================
 
-    brightness = ( rgb[0] +rgb[1] + rgb[2])/3
+# ==================== HELPER FUNCTIONS ====================
+def get_bg(img):
+    '''
+    Determines the background color of the screen
 
-    if brightness < 128: # Obstacle detection
-        pyautogui.press("space")
-        print("Jump")
+    Returns "light" or "dark"
+    '''
+    if (img[:,:,:3].mean(axis=2) > 128).any(): # Obstacle detection based on brightness
+        return "light"
     else:
-        print("Run")
+        return "dark"
+# ==================== HELPER FUNCTIONS ====================
+
+# ==================== MAIN GAME LOOP ====================
+with mss.MSS() as sct:
+    while "Bot Running":
+        x, y = pyautogui.position()
+
+        screen_region = {"top": y - constant, "left": x - constant, "width": 2 * constant, "height": 2 * constant}
+
+        img = np.array(sct.grab(screen_region))
+
+        if get_bg(img) == "light":
+
+            if (img[:,:,:3].mean(axis=2) < 128).any():
+                pyautogui.hotkey("space")
+
+        elif get_bg(img) == "dark":
+            if (img[:,:,:3].mean(axis=2) > 128).any():
+                pyautogui.hotkey("space")
+# ==================== MAIN GAME LOOP ====================
 
